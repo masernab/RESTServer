1ea8bfbda7aca7bb0a2e84ce78c5dd20ebdb1019
Miguel <masernabe@gmail.com>
2021-03-04 10:25:57 -0500

    Primer commit

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { login, googleSingIn } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validar-campos");
+
+const router = Router();
+
+router.post(
+  "/login",
+  [
+    check("correo", "El correo es obligatorio").isEmail(),
+    check("password", "Contraseña obligatoria").notEmpty(),
+    validarCampos,
+  ],
+  login
+);
+
+router.post(
+  "/google",
+  [check("id_token", "id_token obligatoria").notEmpty(), validarCampos],
+  googleSingIn
+);
+
+module.exports = router;
